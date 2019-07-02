c05bbcf74abb2c47d7415915a145e3eefb82a50f
VaiNaWeb <vainaweb@gmail.com>
2019-07-02 15:53:58 -0300

    adding edit to do

--- projetos/todo/main.js
+++ projetos/todo/main.js
@@ -1,72 +1,108 @@
 let form = document.querySelector("form");
 let cards = document.createElement("ul");
 let notes = []
+let isEditing = undefined
 
 form.addEventListener("submit", event => {
 	event.preventDefault();
 
-	notes.push({
+	if (typeof(isEditing) == 'number') {
+		edit()
+	}
+
+	else {
+		notes.push({
+			title: form.elements.title.value,
+			description: form.elements.description.value,
+			category: form.elements.category.value,
+			color: form.elements.color.value,
+		})
+
+		render()
+		cleanValues()
+	}
+});
+
+function edit () {
+	notes[isEditing] = {
 		title: form.elements.title.value,
 		description: form.elements.description.value,
 		category: form.elements.category.value,
 		color: form.elements.color.value,
-	})
+	};
 
-	form.elements.description.value = ''
-	form.elements.category.value = ''
-	form.elements.title.value = ''
-
+	isEditing = undefined;
+	cleanValues()
 	render()
-});
+}
 
 function render() {
 	while (cards.firstChild) {
 		cards.removeChild(cards.firstChild);
 	}
 
-	notes.map((note) => {
+	notes.map((note, index) => {
 		let card = document.createElement("li");
 
 		let title = renderItem("h2", note.title);
 		let description = renderItem("p", note.description);
 		let category = renderItem("p", note.category);
 		let color = renderItem("p", note.color);
-		let button = renderItem("button", "delete");
+
+		let buttons = document.createElement("div");
+
+		let buttonEdit = renderItem("button", "edit");
+		let buttonDelete = renderItem("button", "delete");
 
 		card.appendChild(title);
 		card.appendChild(description);
 		card.appendChild(category);
 		card.appendChild(color);
-		card.appendChild(button);
+		card.appendChild(buttons);
+
+		buttons.appendChild(buttonDelete);
+		buttons.appendChild(buttonEdit);
 
 		card.style.backgroundColor = note.color;
 
 		cards.appendChild(card);
 
-		button.addEventListener("click", function(){
+		buttonDelete.addEventListener("click", function(){
 			notes = arrayRemove(notes, note);
 			render()
 		});
+
+		buttonEdit.addEventListener("click", function(){
+			form.elements.description.value = note.description
+			form.elements.category.value = note.category
+			form.elements.title.value = note.title
+			form.elements.color.value = note.color
+			isEditing = index
+		});
+
 	});
 };
 
 
-function arrayRemove(arr, value) {
-
-   return arr.filter(function(ele){
-       return ele != value;
-   });
-
+function arrayRemove(notes, note) {
+	return notes.filter(function(el){
+		return el != note;
+	});
 }
 
-function renderItem (element, text) {
-	let elementEl = document.createElement(element);
-	let textText = document.createTextNode(text);
+function renderItem (el, txt) {
+	let elementEl = document.createElement(el);
+	let textNode = document.createTextNode(txt);
 
-	elementEl.appendChild(textText)
+	elementEl.appendChild(textNode)
 
 	return elementEl;
 }
 
+function cleanValues () {
+	form.elements.description.value = ''
+	form.elements.category.value = ''
+	form.elements.title.value = ''
+}
 
 document.body.appendChild(cards);
