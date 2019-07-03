0ba761e28a9ab0a276073ed2be2e20329dcb55dc
VaiNaWeb <vainaweb@gmail.com>
2019-07-03 12:32:46 -0300

    mark as checked

--- projetos/todo/main.js
+++ projetos/todo/main.js
@@ -21,56 +21,50 @@ form.addEventListener("submit", event => {
 		render()
 		cleanValues()
 	}
+
 });
-
-function handleEdit () {
-	notes[isEditing] = {
-		title: form.elements.title.value,
-		description: form.elements.description.value,
-		category: form.elements.category.value,
-		color: form.elements.color.value,
-	};
-
-	isEditing = undefined;
-	cleanValues()
-	render()
-}
 
 function render() {
 	while (cards.firstChild) {
 		cards.removeChild(cards.firstChild);
 	}
-
 	notes.map((note, index) => {
-		let card = document.createElement("li");
-		card.style.backgroundColor = note.color;
-
-		let title = renderItem("h2", note.title);
-		let description = renderItem("p", note.description);
-		let category = renderItem("p", note.category);
-		let color = renderItem("p", note.color);
-
-		let buttons = document.createElement("div");
-		let buttonEdit = renderItem("button", "edit");
-		let buttonDelete = renderItem("button", "delete");
-
-		handleEditButton(buttonEdit, note, index)
-		handleDeleteButton(buttonDelete, note)
-
-		buttons.appendChild(buttonDelete);
-		buttons.appendChild(buttonEdit);
-
-		card.appendChild(title);
-		card.appendChild(description);
-		card.appendChild(category);
-		card.appendChild(color);
-		card.appendChild(buttons);
-
-		cards.appendChild(card);
+		renderItem(note, index)
 	});
 };
 
-function renderItem (el, txt) {
+function renderItem(note, index) {
+	let card = document.createElement("li");
+	card.style.backgroundColor = note.check ? '#ddd' : note.color;
+
+	let title = createTag("h2", note.title);
+	let description = createTag("p", note.description);
+	let category = createTag("p", note.category);
+	let color = createTag("p", note.color);
+
+	let buttons = document.createElement("div");
+	let buttonEdit = createTag("button", "edit");
+	let buttonDelete = createTag("button", "delete");
+	let buttonCheck = createTag("button", note.check ? "concluído" : "a fazer");
+
+	handleEditButton(buttonEdit, note, index)
+	handleDeleteButton(buttonDelete, note)
+	handleCheckButton(buttonCheck, note)
+
+	buttons.appendChild(buttonDelete);
+	buttons.appendChild(buttonEdit);
+	buttons.appendChild(buttonCheck);
+
+	card.appendChild(title);
+	card.appendChild(description);
+	card.appendChild(category);
+	card.appendChild(color);
+	card.appendChild(buttons);
+
+	cards.appendChild(card);
+}
+
+function createTag (el, txt) {
 	let elementEl = document.createElement(el);
 	let textNode = document.createTextNode(txt);
 
@@ -79,16 +73,24 @@ function renderItem (el, txt) {
 	return elementEl;
 }
 
+function handleEdit () {
+	notes[isEditing] = {
+		title: form.elements.title.value,
+		description: form.elements.description.value,
+		category: form.elements.category.value,
+		color: form.elements.color.value,
+		check: notes[isEditing].check ? notes[isEditing].check : undefined
+	};
+
+	isEditing = undefined;
+	cleanValues()
+	render()
+}
+
 function handleDeleteButton(buttonDelete, note, index) {
 	buttonDelete.addEventListener("click", function(){
 		notes = arrayRemove(notes, note);
 		render()
-	});
-}
-
-function arrayRemove(notes, note) {
-	return notes.filter(function(el){
-		return el != note;
 	});
 }
 
@@ -99,6 +101,19 @@ function handleEditButton(buttonEdit, note, index) {
 		form.elements.title.value = note.title
 		form.elements.color.value = note.color
 		isEditing = index
+	});
+}
+
+function handleCheckButton (buttonCheck, note) {
+	buttonCheck.addEventListener("click", function() {
+		note.check = !note.check
+		render()
+	})
+}
+
+function arrayRemove(notes, note) {
+	return notes.filter(function(el){
+		return el != note;
 	});
 }
 
